155c36a38bbfb01aa53c2b6d63a5c84c0a578b47
Wong Yong Jie <yjwong92@gmail.com>
2018-09-18 11:14:15 +0800

    Initial commit.

---
+++ jsx-control-statements.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    'jsx-control-statements/jsx-control-statements': true,
+  },
+  rules: {
+    'jsx-control-statements/jsx-choose-not-empty': 1,
+    'jsx-control-statements/jsx-for-require-each': 1,
+    'jsx-control-statements/jsx-for-require-of': 1,
+    'jsx-control-statements/jsx-if-require-condition': 1,
+    'jsx-control-statements/jsx-otherwise-once-last': 1,
+    'jsx-control-statements/jsx-use-if-tag': 1,
+    'jsx-control-statements/jsx-when-require-condition': 1,
+    'jsx-control-statements/jsx-jcs-no-undef': 0
+  },
+  extends: [
+    'plugin:jsx-control-statements/recommended',
+  ],
+  plugins: [
+    'jsx-control-statements',
+  ],
+};
---
+++ react.js
@@ -0,0 +1,38 @@
+module.exports = {
+  rules: {
+    // React
+    'react/display-name': 'warn',
+    'react/no-deprecated': 'warn',
+    'react/no-did-mount-set-state': 'error',
+    'react/no-did-update-set-state': 'error',
+    'react/no-direct-mutation-state': 'error',
+    'react/no-is-mounted': 'error',
+    'react/no-set-state': 'error',
+    'react/no-string-refs': 'error',
+    'react/no-unknown-property': 'warn',
+    'react/prefer-es6-class': 'warn',
+    'react/prefer-stateless-function': 'warn',
+    'react/prop-types': 'warn',
+    'react/react-in-jsx-scope': 2,
+    'react/require-render-return': 'error',
+    'react/self-closing-comp': 'warn',
+
+    // React/JSX
+    'react/jsx-boolean-value': ['warn', 'always'],
+    'react/jsx-equals-spacing': 'error',
+    'react/jsx-indent': ['warn', 2],
+    'react/jsx-key': 'warn',
+    'react/jsx-no-duplicate-props': 'error',
+    'react/jsx-no-undef': ['error', { allowGlobals: true }],
+    'react/jsx-pascal-case': 'warn',
+    'react/jsx-tag-spacing': ['warn', {
+      beforeSelfClosing: 'always',
+    }],
+    'react/jsx-uses-react': 2,
+    'react/jsx-uses-vars': 2,
+    'react/jsx-wrap-multilines': 'warn',
+  },
+  plugins: [
+    'react',
+  ],
+};
